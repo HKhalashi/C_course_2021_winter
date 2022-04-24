e5b85f0b8bcdbd302d2d64945127a2c31ed9db6c
HKhalashi <mohammadhosseinkhalashi@gmail.com>
2022-04-24 19:29:45 +0300

    pointer3_1.c formated and fixed

--- pointer3_1.c
+++ pointer3_1.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <string.h>
-int main(int argc,const char *argv[])
-{
-    int total = 0;
-   for(int i =0 ; i < argc ; i++){
-    total = total + (int)strlen(argv[i]);
-       }
-    printf("argc = %d\n",argc);
-    printf("total = %d\n", total);
-    char a[total];
-    for(int i = 0; i < argc ; i++){
-    strcat(a , argv[i]);
-    }
-    printf("%s\n", a);
+
+int main(int argc,
+  const char * argv[]) {
+  int total = 0;
+  for (int i = 0; i < argc; i++) {
+    total = total + (int) strlen(argv[i]);
+  }
+  printf("argc = %d\n", argc);
+  printf("total = %d\n", total);
+  char a[total];
+  a[0] = '\0';
+  for (int i = 0; i < argc; i++) {
+    strcat(a, argv[i]);
+  }
+  printf("%s\n", a);
 
   return 0;
 }
